514e2f720a35c40d2ce6ddb973076b406b598fda
Juan Pablo <jpschiaffino@gmail.com>
2022-05-01 19:55:32 -0400

    more changes in icons of powerline in qtile

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -17,7 +17,7 @@ from .keys import mod, keys
 # nf-mdi-layers
 
 groups = [Group(i) for i in [
-    "   ", "   ", "   ", " 溜  ", "   ", "   ", "   ", "   ", "   ",
+    "   ", "   ", "   ", " 溜  ", "   ", "   ", "   ", "   ", " 甆  ",
 ]]
 
 for i, group in enumerate(groups):
