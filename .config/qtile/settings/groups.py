d440eab70c9c1f79a3f3f85e04a3cd75a9b39d56
Juan Pablo <jpschiaffino@gmail.com>
2022-05-06 21:44:00 -0400

    change icons for qtile

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -17,7 +17,7 @@ from .keys import mod, keys
 # nf-mdi-layers
 
 groups = [Group(i) for i in [
-    "   ", "   ", "   ", " 溜  ", "   ", "   ", "   ", "   ", " 甆  ",
+    "   ", "   ", "   ", " 溜  ", "   ", "   ", "   ", "   ", "   ",
 ]]
 
 for i, group in enumerate(groups):
